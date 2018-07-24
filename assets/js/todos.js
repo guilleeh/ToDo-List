055c8c9c4e62b37be688cd6cbfad327302bfdc68
Guillermo <guilleeh@uci.edu>
2018-07-24 16:19:05 -0700

    Task addition and turn on/off implementation

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,20 @@
-alert("CONNECTED!");
+// alert("CONNECTED!");
+
+
+
+//Toggling done/not done
+$("li").click(function() {
+	$(this).toggleClass("done");
+})
+
+//adding tasks
+$("input").keypress(function (e) {
+	if( e.keyCode == 13 ) {
+		console.log($(this));
+		$("ul").append(`<li>${$(this).val()}</li>`);
+		$("input").val('');
+	}
+});
+
+//deleting items
+
